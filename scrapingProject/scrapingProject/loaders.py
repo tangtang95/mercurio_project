ba3a7d7e7bed35fed79ced101dc1797756f4680b
Tang-Tang Zhou <tangtang.zhou@mail.polimi.it>
2018-03-21 16:31:17 +0100

    added bloombergspider
    features:
    - parsing news
    - cleaning page content
    - write tsv file

---
+++ scrapingProject/scrapingProject/loaders.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags, replace_escape_chars, strip_html5_whitespace
+
+class NewsLoader(ItemLoader):
+    
+    default_output_processor = TakeFirst()
+
+    title_in = MapCompose(remove_tags)
+    title_out = TakeFirst()
+
+    author_in = MapCompose(strip_html5_whitespace, replace_escape_chars, remove_tags)
+    author_out = TakeFirst()
+    
+    date_in = MapCompose(remove_tags)
+    date_out = TakeFirst()
+    
+    time_in = MapCompose(remove_tags)
+    time_out = TakeFirst()
+    
+    #content_in = MapCompose(remove_tags_with_content, which_ones = ('style', ))
+    #content_out = MapCompose(remove_tags)
--- scrapingProject/scrapingProject/settings.py
+++ scrapingProject/scrapingProject/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapingProject.pipelines.ScrapingprojectPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'scrapingProject.pipelines.ScrapingprojectPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
