dec48470bd6a3fa518dacac99c36e98dbb5fcdb4
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-03-23 23:30:38 +0100

    Implementazione parziale del la rimozione del codice HTML
    Leggere il commento riguardante lo stato dell'arte

--- scrapingProject/scrapingProject/spiders/nytimespider.py
+++ scrapingProject/scrapingProject/spiders/nytimespider.py
@@ -15,10 +15,12 @@ class NYTimesSpider(scrapy.Spider):
     CORRECTIVE_FACTOR = 0.95
     #MAX_ITERATION = NUM_YEAR / AVG_DAY_PER_SCROLL * CORRECTIVE_FACTOR * 365
     MAX_ITERATION = 10
+    year = "0"
     
     '''
         Filtering news based on the black_world_list.
-        If black_world_list isn't defined, return the original list
+        If black_world_list isn't defined, return the original list.
+        The list is a list of WebElement.
     ''' 
     def filter_news_blacklist(self, links):
         if self.black_word_list == None:
@@ -33,7 +35,8 @@ class NYTimesSpider(scrapy.Spider):
         
     '''
         Filtering news based on a list of allowed word.
-        If allowed_word_list is not defined, return an empty list
+        If allowed_word_list is not defined, return an empty list.
+        The list is a list of WebElement.
     
         TO FIX
         Qui sembra esserci un bug riguardante l'ultimo elemento analizzato
@@ -55,14 +58,14 @@ class NYTimesSpider(scrapy.Spider):
             for word in self.allowed_word_list:
                 if word in item:
                     flag = True
+                    break
             if flag == False:
                 links.remove(item)
         
     
     '''
         Write on file the list of the scraped sites 
-        
-        NEED A FIX, Error writing file
+        The list is a list of WebElement
     '''
     def write_file(self, file_name, list1):
         try:
@@ -72,11 +75,11 @@ class NYTimesSpider(scrapy.Spider):
             return
         try:        
             for t in list1:
-                if self.filter_news(t.get_attribute("href")) == False:
-                    out_file.write(t.get_attribute("href") + "\n")
+                out_file.write(t.get_attribute("href") + "\n")
             out_file.write("\n\n\n")
-        except: 
+        except Exception as e:
             self.logger.error("Error writing file: " + file_name)
+            self.logger.error(e)
         finally: 
             out_file.close()
     
@@ -97,6 +100,18 @@ class NYTimesSpider(scrapy.Spider):
         della stima del numero di iterazioni. Probabilmente è il migliore da
         tenere in considerazione, dato che l'altro consente di eseguire
         uno script piu compatto.
+        
+        
+        STATO DELL'ARTE E ALTERNATIVE PERCORSE
+        Essenzialmente per rimuovere elementi bisogna utilizzare degli script 
+        javascript, tuttavia i vari articoli sono identificati solamente dall'id
+        di conseguenza (oppure dal tag name, tuttavia, avendo provato affermo
+        che non ha senso rimuovere per tag name in quanto poi non è piu' possibile
+        continuare nello scroll). Di conseguenza la strada che qui si tenta è
+        quella di rimuovere tutti gli articoli tramite il loro id, ottenuto tramite
+        una query con xpath. 
+        Resta un problema sul latest-page-marker che a volte viene cancellato 
+        e rimosso dal DOM impedendo di scrollare ancora piu in basso.
     '''
     def parse(self, response):
         driver = webdriver.Firefox()
@@ -111,25 +126,46 @@ class NYTimesSpider(scrapy.Spider):
         flag = False
         i = 0
         try:   
-            while flag == False and i < self.MAX_ITERATION: #to_change_with: or
+            while flag == False and i < self.MAX_ITERATION: #to_change_with: or in non debug case
+                # Handling infinite scroll with both method
                 last_count = page_len
                 time.sleep(SCROLL_PAUSE_TIME)
                 page_len = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var page_len=document.body.scrollHeight;return page_len;")
                 if last_count == page_len:
                     flag = True
-                i = i + 1    
-        except:
+                i = i + 1 
+                
+                '''
+                LEGGI STATO NELL'ARTE NEL COMMENTO SOPRA
+                '''
+                # Trying to implement html code remove
+                # Retriving and writing articles' link
+                articles_added = driver.find_elements_by_xpath(".//section[@id = 'latest-panel']//a[@class='story-link']")
+                self.write_file("nytimes_news", articles_added)
+                
+                # Retriving articles' id to remove
+                id_list = driver.find_elements_by_xpath('.//ol[@id="story-menu-additional-set-latest"]/*')
+                id_list = id_list[int(len(id_list)/2):]
+                # Removing articles from the page by their id
+                for item in id_list:
+                    item = item.get_attribute("id")
+                    if item != "latest-page-marker":
+                        driver.execute_script('x=document.getElementById(\"'+item+'\"); x.parentNode.removeChild(x);')
+        except Exception as e:
             self.logger.error("Error scraping dealbook section of nytimes.com")
+            self.logger.error(e)
         finally:    
-            story_links = driver.find_elements_by_xpath(".//section[@id = 'latest-panel']//a[@class='story-link']")
+            #story_links = driver.find_elements_by_xpath(".//section[@id = 'latest-panel']//a[@class='story-link']")
+            # Stupid action in order to keep 'finally' not commented
+            story_links = None
             
             # Filtering links
-            headline_links = self.filter_news_allowed_section(headline_links)
-            story_links = self.filter_news_allowed_section(story_links)
+            #self.filter_news_allowed_section(headline_links)
+            #self.filter_news_allowed_section(story_links)
             
             # Writing file
-            self.write_file("nytimes_news", headline_links)
-            self.write_file("nytimes_news", story_links)
+            #self.write_file("nytimes_news", headline_links)
+            #self.write_file("nytimes_news", story_links)
         
             
         
