cc21d5ffa18815c1c35ed95ffd61e3fc2c09f288
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-03-27 21:12:23 +0200

    Market watch implemented with html delete

--- scrapingProject/scrapingProject/spiders/marketwatchspider.py
+++ scrapingProject/scrapingProject/spiders/marketwatchspider.py
@@ -8,7 +8,7 @@ class NYTimesSpider(scrapy.Spider):
     name = "marketwatchspider"
     allowed_domains = ['www.marketwatch.com/']
     start_urls = ['https://www.marketwatch.com/newsviewer']
-    MAX_ITERATION = 2
+    MAX_ITERATION = 15000
     
     def parse(self, response):
         driver = webdriver.Firefox()
@@ -16,31 +16,44 @@ class NYTimesSpider(scrapy.Spider):
         
         driver.execute_script("x = document.getElementById('mktwheadlines').getElementsByClassName('viewport')[0];")
         
+        #removing unncesserary stuff from the page
+        driver.execute_script('x=document.getElementById("thirdpartyheadlines"); x.parentNode.removeChild(x);')
+        driver.execute_script('x=document.getElementById("rightrail"); x.parentNode.removeChild(x);')
+        driver.execute_script('x=document.getElementById("thirdpartycontrols"); x.parentNode.removeChild(x);')
+        driver.execute_script('x=document.getElementById("sponsoredlinks"); x.parentNode.removeChild(x);')
+        driver.execute_script('x=document.getElementById("below"); x.parentNode.removeChild(x);')
+        driver.execute_script('x=document.getElementById("chrome"); x.parentNode.removeChild(x);')
+        
         i = 0
+        # Infinite scrolling
         try:   
             while i < self.MAX_ITERATION: 
                 time.sleep(SCROLL_PAUSE_TIME)
                 driver.execute_script("x = document.getElementById('mktwheadlines').getElementsByClassName('viewport')[0];x.scrollTo(0,x.scrollHeight);")
-                i = i + 1 
+                i = i + 1
+                    
+                # Every ten iteration, remove the <li> tags from the web page
+                if i % 10 == 0:
+                    elements = driver.find_elements_by_xpath('.//div[@id="mktwheadlines"]//ol[@class="viewport"]/li')
+                    # Printing retrived elements to the file
+                    item = BriefItem()
+                    for elem in elements:
+                        try:
+                            element_data = elem.get_attribute("timestamp")
+                            item['title'] = elem.find_element_by_xpath('.//div[@class="nv-text-cont"]').text
+                            item['url'] = elem.find_element_by_xpath('.//a[@class="read-more"]').get_attribute("href")
+                            temp = element_data.split(" ")
+                            item['date'] = temp[0]
+                            item['time'] = temp[1] + " " + temp[2]
+                            
+                            yield item        
+                        except Exception as e:
+                            pass
+                    driver.execute_script('var element = document.getElementsByTagName("li"); var index;for (index = 0; index <= element.length - 2; index++) {element[index].parentNode.removeChild(element[index]);}')
         except Exception as e:
             self.logger.error("Error scraping dealbook section of nytimes.com")
             self.logger.error(e)
         finally:    
-            # Retriving data
-            elements = driver.find_elements_by_xpath('.//div[@id="mktwheadlines"]//ol[@class="viewport"]/li')
-            
-            # Gettin the value of data and cont for each section
-            item = BriefItem()
-            for elem in elements:
-                try:
-                    element_data = elem.get_attribute("timestamp")
-                    item['title'] = elem.find_element_by_xpath('.//div[@class="nv-text-cont"]').text
-                    item['url'] = elem.find_element_by_xpath('.//a[@class="read-more"]').get_attribute("href")
-                    temp = element_data.split(" ")
-                    item['date'] = temp[0]
-                    item['time'] = temp[1] + " " + temp[2]
-                    
-                    yield item
-                    
-                except Exception as e:
-                    pass
+            # need to close the driver
+            #driver.close()
+            pass
