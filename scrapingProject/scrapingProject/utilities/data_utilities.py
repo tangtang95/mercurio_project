58cd5b5c46160eb0dc82e4dcaf9f6dde08a0cb9d
Tang-Tang Zhou <tangtang.zhou@mail.polimi.it>
2018-04-05 23:57:37 +0200

    implemented normalization of timestamp with standard format and timezone and refactored the two marketwatch spiders

--- scrapingProject/scrapingProject/utilities/data_utilities.py
+++ scrapingProject/scrapingProject/utilities/data_utilities.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+from dateutil import parser
+import pytz
 
+#DEPRECATED
 def zero_pad_timestamp(timestamp):
     '''
     Given a timestamp in the format
@@ -15,20 +18,31 @@ def zero_pad_timestamp(timestamp):
             x[i][0] = "0"+x[i][0]   
             
     return ""+x[0][0]+"/"+x[0][1]+"/"+x[0][2]+" "+x[1][0]+":"+x[1][1]+":"+x[1][2]+" "+x[2]
-           
+
+
+
+def normalize_timestamp(date, timezone = 'UTC', output_format = '%Y-%m-%d %H:%M:%S'):
+    '''
+    Return a normalized date string with:
+    - a standard format: output_format
+    - a standard timezone: UTC
+    '''
+    other_tz = pytz.timezone(timezone)
+    utc = pytz.timezone('UTC')
+    timestamp = parser.parse(date)
+    timestamp = other_tz.localize(timestamp).astimezone(utc)
+    return timestamp.strftime(output_format)
     
-def compareTime(ts1, ts2):
+
+def compare_time(ts1, ts2):
     '''
     Compare timestamp ts1 and ts2. 
     If ts1 < ts2 then return True; false otherwise.
-    ts1 and ts2 format must be:
-        
-    3/27/2018 4:01:29 PM
     '''
-    ts1 = zero_pad_timestamp(ts1)
-    ts2 = zero_pad_timestamp(ts2)
-    t1 = datetime.strptime(ts1, "%m/%d/%Y %I:%M:%S %p")
-    t2 = datetime.strptime(ts2, "%m/%d/%Y %I:%M:%S %p")
+    ts1 = normalize_timestamp(ts1)
+    ts2 = normalize_timestamp(ts2)
+    t1 = datetime.strptime(ts1, '%Y-%m-%d %H:%M:%S')
+    t2 = datetime.strptime(ts2, '%Y-%m-%d %H:%M:%S')
         
     if t1 < t2:
         return True
