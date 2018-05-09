29b49a4871764bf31bf15080da4ecfd9267ba1ab
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-05-09 18:05:41 +0200

    Refactor weight function

--- analysis/sentiment/running_scripts.py
+++ analysis/sentiment/running_scripts.py
@@ -2,6 +2,8 @@
 '''
 Files used to run some temporary scripts regarding utilies, like files 
 operation that need to be done only once
+
+This is an example of how to use sentiment_function
 '''
 import utilities.functions as functions
 import sentiment.sentiment_function as sfu
