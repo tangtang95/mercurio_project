3ae41bca59cefd5e8d6eaf69f5298b291f551627
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-05-21 16:14:34 +0200

    sentiment main

---
+++ analysis/sentiment_main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import sys
+from sentiment import sentiment_function as sf
+from utilities import articlesDAO as dao
+from utilities.database import Database
+from utilities import functions as fu
+from stanfordcorenlp import StanfordCoreNLP
+
+def main():
+    try:
+        db = Database()
+        db.open_connection()
+        analyzed_dao = dao.ArticleAnalyzedDAO(db.get_database())
+        result = analyzed_dao.getArticlesByNewsPaper(sys.argv[1])        
+        vocabulary = fu.getVocabulary()
+        with StanfordCoreNLP('http://localhost', port=9001, memory='4g') as nlp:
+            for row in result:
+                sentiment = sf.analyze_article(row[5], vocabulary, nlp)
+                analyzed_dao.updateNewsSentiment(row[0], sentiment)
+    except Exception as err:
+        raise Exception(err)
+    finally:
+        db.close_connection()
+        
+if __name__ == "__main__":
+    main()
+            
